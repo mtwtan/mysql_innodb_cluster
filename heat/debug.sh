d841677a9b7ba91d905b2340424079778296b751
mtwtan <mtwtan@gmail.com>
2018-11-20 22:52:57 -0500

    Update debug.sh

--- heat/debug.sh
+++ heat/debug.sh
@@ -3,11 +3,11 @@
 time openstack stack create \
   -e mysql_env.yaml \
   --parameter node_count=3 \
-  --parameter image=CentOS7-4 \
-  --parameter flavor=p4.medium \
-  --parameter key_name=INE \
-  --parameter mysql_network=d1a34821-8a48-4739-b468-c6fca61e514c \
-  --parameter mysql_security_group=SG_MYSQL \
+  --parameter image=IMAGE_NAME \
+  --parameter flavor=FLAVOR_NAME \
+  --parameter key_name=KEY_NAME \
+  --parameter mysql_network=NETWORK_ID \
+  --parameter mysql_security_group=SECURITY_NAME \
   --parameter mysql_volume_size=500 \
   -t mysql_stack.yaml \
-  fde-mysql-stack
+  name-of-stack
